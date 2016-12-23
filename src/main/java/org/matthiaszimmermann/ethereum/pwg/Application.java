67b4a00e474acba0ba8eb013a625274efd8f3551
Matthias Zimmermann <matthias.zimmermann@bsiag.com>
2016-12-23 12:02:46 +0100

    update todo list

--- src/main/java/org/matthiaszimmermann/ethereum/pwg/Application.java
+++ src/main/java/org/matthiaszimmermann/ethereum/pwg/Application.java
@@ -6,15 +6,11 @@ public class Application {
 		
 		// TODO add optional parameter (pass phrase)
 		// TODO add optional parameter (target directory)
+		// TODO test + write more tests
+		// TODO add more docu
 		
 		PaperWallet pw = new PaperWallet(null, null);
 		String html = WalletPageUtility.createHtml(pw);
 		FileUtility.saveToFile(html, "testli.html");
-		
-		// TODO create github repo
-		// TODO test + write more tests
-		// TODO add docu
-		// TODO add tweet
-		
 	}
 }
